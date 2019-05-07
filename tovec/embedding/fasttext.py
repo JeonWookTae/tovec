084dd5efa8329d93a74990ffd5f4c48383415022
JeonWookTae <pkgg011@naver.com>
2019-05-07 10:33:51 +0900

    gensim train update

--- tovec/embedding/fasttext.py
+++ tovec/embedding/fasttext.py
@@ -20,6 +20,25 @@ class Fasttext(object):
                 vector_list.append(np.zeros(300) + 1e-7)
         return vector_list
 
+    @classmethod
+    def trainer(cls, corpus):
+        """
+        :param corpus: [sentence: [word, word, . . ., word]]
+        :return:
+        """
+        train_instance = FastText(sentences=corpus, size=300, window=5, min_count=1)
+        cls.model = train_instance
+
+    def updater(self, corpus):
+        if self.model:
+            self.model.build_vocab(corpus, update=True)
+            self.model.train(corpus, total_examples=len(corpus), epochs=10)
+
     def __call__(self, phrase):
         phrase = self.conver_list(phrase=phrase)
         return self.vectorize(token_list=phrase)
+
+
+if __name__ == '__main__':
+    Fasttext.trainer([['하이','안녕'], ['반가워','하이루'],['이거','이렇게','해야']])
+
